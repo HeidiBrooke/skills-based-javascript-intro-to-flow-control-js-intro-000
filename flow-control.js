074264f7501a9dad27ce97b1ffa3a4d906f51928
Heidi B McGinnis <heidiisbrooke@gmail.com>
2020-10-25 23:16:27 +0000

    Automatically backed up by Learn

--- flow-control.js
+++ flow-control.js
@@ -21,7 +21,7 @@ function ageChecker(age) {
     return "You are a teenager!"
 
   } else if (age < 13 ){
-    return "You are a kid."
+    return "You are a kid"
 
   }
   else { return "You are a grownup."}
